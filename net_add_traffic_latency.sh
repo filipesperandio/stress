abda6d3942942f3f95b32c1e3c195f667fd62de0
Filipe Gomes Esperandio <filipesperandio@gmail.com>
2013-08-08 15:45:43 -0300

    fix if

--- net_add_traffic_latency.sh
+++ net_add_traffic_latency.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 
-[[ ! ${1} ]] && echo "Usage: ${0} 500ms" && exit -1 
+[ ! ${1} ] && echo "Usage: ${0} 500ms" && exit -1 
 
 tc qdisc del dev eth0 root
 tc qdisc add dev eth0 root netem delay ${1}
